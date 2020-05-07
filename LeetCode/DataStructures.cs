dcffefc708701d47a4a3ed9d2ebbeca7f4fc2c42
Wenkan ZHOU <zhouwenkan@fotoable.com>
2020-05-07 16:35:04 +0800

    Add TreeNode, ListNode, while loop in main

---
+++ LeetCode/DataStructures.cs
@@ -0,0 +1,13 @@
+﻿
+public class TreeNode {
+    public int val;
+    public TreeNode(int val) => this.val = val;
+    public TreeNode left;
+    public TreeNode right;
+}
+
+public class ListNode {
+    public int val;
+    public ListNode(int val) => this.val = val;
+    public ListNode next;
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LeetCode {
-    class Program {
-        static void Main(string[] args) {
+public class Solution {
+    public int[] LevelOrder(TreeNode root) {
+        var result = new List<int>();
+        if (root == null) return result.ToArray();
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        while (queue.Count > 0) {
+            var node = queue.Dequeue();
+            result.Add(node.val);
+            if (node.left != null) {
+                queue.Enqueue(node.left);
+            }
+            if (node.right != null) {
+                queue.Enqueue(node.right);
+            }
         }
+        return result.ToArray();
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+
+        while (true) {
+            var inputString = Console.ReadLine();
+            if (inputString == "exit") break;
+
+            try {
+                var root = LeetCodeUtil.ParseTreeNode(inputString);
+                var s = new Solution();
+                var result = s.LevelOrder(root);
+                LeetCodeUtil.Dump(result);
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+        Console.ReadLine();
     }
 }
